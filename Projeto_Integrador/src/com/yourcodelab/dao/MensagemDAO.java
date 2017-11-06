afa50d0a644a1c6dab0baac2a68b40ca52d6702a
Programador <Programador@192.168.88.132>
2017-11-06 21:56:29 -0200

    ATUALIZADO 06/11/2017

--- Projeto_Integrador/src/com/yourcodelab/dao/MensagemDAO.java
+++ Projeto_Integrador/src/com/yourcodelab/dao/MensagemDAO.java
@@ -22,7 +22,7 @@ public class MensagemDAO extends GenericDAO{
 	private String LISTAR_MENSAGEM = "select tb_mensagem.*, tb_resposta.id as idResposta, tb_resposta.description from tb_mensagem inner join tb_resposta on tb_mensagem.idresposta = tb_resposta.id;";
 	private String EXCLUIR = "DELETE FROM  tb_mensagem WHERE ID=?;";
 	private String ALTERAR = "UPDATE tb_mensagem SET nomeempresa= ?, email= ?, msg = ?,idresposta=?,endereco=?,vaga=?,horaentrevista=?,data=? WHERE id=?;";
-	
+	private String teste = "DELETE FROM  tb_mensagem WHERE ID=?;";
 	
 	
 	public void salvar(Mensagem c) throws SQLException, ClassNotFoundException, IOException {
